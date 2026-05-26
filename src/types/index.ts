fb535da74ba854fcd64da2010f4d5016a3f216b6
VIVEK KUMAR <143505810+Vroy4298@users.noreply.github.com>
2026-05-26 23:46:50 +0530

    added base types and entry point setup

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+export interface GainLoss {
+  balance: number;
+  gain: number;
+}
+
+export interface Holding {
+  coin: string;
+  coinName: string;
+  logo: string;
+  currentPrice: number;
+  totalHolding: number;
+  averageBuyPrice: number;
+  stcg: GainLoss;
+  ltcg: GainLoss;
+}
+
+export interface CapitalGainCategory {
+  profits: number;
+  losses: number;
+}
+
+export interface CapitalGains {
+  stcg: CapitalGainCategory;
+  ltcg: CapitalGainCategory;
+}
+
+export interface CapitalGainsResponse {
+  capitalGains: CapitalGains;
+}
+
+export interface ComputedGains {
+  stcg: CapitalGainCategory;
+  ltcg: CapitalGainCategory;
+  netStcg: number;
+  netLtcg: number;
+  realisedGains: number;
+}
